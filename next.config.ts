d727aecee8a5824c047b6f051d0bb28490385645
bangnevgo <bangnevgo@gmail.com>
2026-05-30 18:50:19 +0700

    fix: add standalone output for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,2 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: false,
-  trailingSlash: true,
-};
-
-export default nextConfig;
+const nextConfig = { output: 'standalone' }
+export default nextConfig
